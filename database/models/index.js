d3c1bf081246f66b7be1d57e4778deaa1e28d87a
josemak25 <amakirij@gmail.com>
2020-01-23 11:01:06 +0100

    add default export for all databse models

---
+++ database/models/index.js
@@ -0,0 +1,7 @@
+// models/index.js
+import Games from "./games";
+import Genres from "./genres";
+import Screenshots from "./screenshots";
+import Videos from "./videos";
+
+export default dbModels = [Games, Genres, Screenshots, Videos];
