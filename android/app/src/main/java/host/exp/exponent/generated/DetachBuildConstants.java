fe2497794ff6d65846a3e96f9e58fe25f078b833
josemak25 <amakirij@gmail.com>
2020-01-27 09:51:44 +0100

    add eject for android and ios folder

---
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -0,0 +1,8 @@
+package host.exp.exponent.generated;
+
+// This file is auto-generated. Please don't rename!
+public class DetachBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "expf71e353ae35e4883877c6b061de31caa://192.168.88.171:19000";
+
+}
