e35616d273b9aaa9405e29f94213b11bde7ae158
josemak25 <amakirij@gmail.com>
2020-01-28 14:24:27 +0100

    add conditional to home page render component

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useEffect, useContext } from "react";
+import LoadingGames from "../../components/LoadingGames";
+import { StoreContext } from "../../store";
+import loadGames from "../../store/actions";
+
 import { Container, PleaseDeleteMe } from "./styles";
 
-export default function Home() {
-  return (
+export default function Home({ navigation }) {
+  const [store, dispatch] = useContext(StoreContext);
+
+  useEffect(() => {
+    loadGames(dispatch);
+  }, []);
+
+  return !store.isLoading ? (
     <Container>
       <PleaseDeleteMe>
         Open up App.js to start working on your app!
       </PleaseDeleteMe>
     </Container>
+  ) : (
+    <LoadingGames />
   );
 }
 
