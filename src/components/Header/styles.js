b6aae35abbdd8ac7ce57cbcbd59324622f76dc3b
josemak25 <amakirij@gmail.com>
2020-02-01 20:48:39 +0100

    add header component styles

---
+++ src/components/Header/styles.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+import { colors, fonts } from "../../constants";
+
+export const Container = styled.View`
+  width: 100%;
+  height: 150%;
+  justify-content: flex-end;
+  padding-bottom: 30px;
+  background-color: ${colors.ICON_DARK_COLOR};
+`;
+
+export const HeaderContents = styled.View`
+  width: 100%;
+  height: 60%;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 10px;
+  background-color: ${colors.ICON_DARK_COLOR};
+`;
