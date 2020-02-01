66d812fd29de2c84063917bea8f6d35c5804572b
josemak25 <amakirij@gmail.com>
2020-02-01 20:47:31 +0100

    add menu component

---
+++ src/components/Menu/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Container } from "./styles";
+import MenuIcon from "../../../assets/icons/menu";
+
+export default function Menu() {
+  return (
+    <Container>
+      <MenuIcon />
+    </Container>
+  );
+}
