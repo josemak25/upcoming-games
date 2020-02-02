c8f7fb312f1860e0eda980140426e35c2ec6ba55
josemak25 <amakirij@gmail.com>
2020-02-02 08:13:20 +0100

    add chanage to query and temp game list on testing

--- src/utils/fetchGames.js
+++ src/utils/fetchGames.js
@@ -1,11 +1,11 @@
 import APP_CONFIG from "../config";
 
-const QUERY_FIELDS = `fields name, platforms, rating, url, videos.*, release_dates, genres.*, summary, screenshots.*; 
-                      where platforms = 48 & release_dates.date > 1559343600;
-                      sort release_dates.date asc; limit 5; 
-                      exclude platforms;`;
+export default fetchGames = ({ year = 2019, platform = 48 }) => {
+  const QUERY_FIELDS = `fields name, platforms, rating, url, videos.*, release_dates.*, genres.*, summary, screenshots.*;
+                          where platforms = ${platform} & release_dates.y > ${year} & screenshots >= 1; sort release_dates.date asc;
+                          limit 15;
+                          exclude platforms ;`;
 
-export default fetchGames = () => {
   return fetch(`${APP_CONFIG.DATABASE_URI}/games`, {
     method: "POST",
     headers: { Accept: "application/json", "user-key": APP_CONFIG.API_KEY },
