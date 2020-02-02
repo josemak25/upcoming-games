6b66e0a4b200ad01bdc9bb9e5f95e49cc3bd254f
josemak25 <amakirij@gmail.com>
2020-02-02 08:18:02 +0100

    add game release date to shema export

--- src/database/schema/index.js
+++ src/database/schema/index.js
@@ -4,5 +4,13 @@ import Genres from "./genres";
 import Screenshots from "./screenshots";
 import Videos from "./videos";
 import Favorites from "./favorites";
+import ReleaseDate from "./releaseDate";
 
-export default dbSchemas = [Games, Genres, Screenshots, Videos, Favorites];
+export default dbSchemas = [
+  Games,
+  Genres,
+  Screenshots,
+  Videos,
+  Favorites,
+  ReleaseDate
+];
