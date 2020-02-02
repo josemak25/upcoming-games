77c7f0a58eca2b74e7728e92f2f8c158a12df5fc
josemak25 <amakirij@gmail.com>
2020-02-02 08:17:31 +0100

    add game release date schema

---
+++ src/database/schema/releaseDate.js
@@ -0,0 +1,19 @@
+export const RELEASE_DATE_SCHEMA = "ReleaseDate";
+
+export default ReleaseDateSchema = {
+  primaryKey: "id",
+  name: RELEASE_DATE_SCHEMA,
+  properties: {
+    id: "int",
+    category: "int",
+    created_at: "int",
+    date: "int",
+    game: "int",
+    human: "string",
+    m: "int",
+    platform: "int",
+    region: "int",
+    updated_at: "int",
+    y: "int"
+  }
+};
