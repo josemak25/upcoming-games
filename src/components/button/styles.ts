5ecb81995061fcc8ea1f224a55862c0b1e466086
josemak25 <amakirij@gmail.com>
2020-05-20 11:21:18 +0100

    removed fixed minimum button height and width

--- src/components/button/styles.ts
+++ src/components/button/styles.ts
@@ -1,17 +1,15 @@
 import styled from 'styled-components/native';
 
 export const Container = styled.TouchableOpacity`
-  min-width: 100px;
-  min-height: 50px;
   padding: 15px;
   justify-content: center;
   align-items: center;
   background-color: ${({ theme }) => theme.colors.ACTION_BG_COLOR};
-  border-radius: 5px;
+  border-radius: 50px;
 `;
 
 export const ButtonText = styled.Text`
-  font-size: ${({ theme }) => theme.fonts.LARGE_SIZE}px;
+  font-size: ${({ theme }) => theme.fonts.MEDIUM_SIZE}px;
   font-family: ${({ theme }) => theme.fonts.NOTOSANS_BOLD};
   color: ${({ theme }) => theme.colors.WHITE_BG_COLOR};
   text-transform: capitalize;
