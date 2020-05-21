7fbb906b5d7bb7ada9f33e4d98452fea867937b9
josemak25 <amakirij@gmail.com>
2020-05-21 09:26:42 +0100

    add home screen styles

--- src/screens/home/styles.ts
+++ src/screens/home/styles.ts
@@ -5,3 +5,11 @@ export const Container = styled.View`
   align-items: center;
   background-color: ${({ theme }) => theme.colors.DARK_BG_COLOR};
 `;
+
+export const HeaderTitle = styled.Text`
+  font-size: ${({ theme }) => theme.fonts.LARGE_SIZE + 10}px;
+  font-family: ${({ theme }) => theme.fonts.BEAUTY_MOUNTAINS};
+  color: ${({ theme }) => theme.colors.BLACK_FONT_COLOR};
+  text-transform: capitalize;
+  line-height: 50px;
+`;
