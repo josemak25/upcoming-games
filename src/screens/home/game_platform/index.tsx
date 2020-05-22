381ea3950cf3e0204ac7915eb202551bf2a1608b
josemak25 <amakirij@gmail.com>
2020-05-22 10:49:55 +0100

    add game platform component and implement recycler list view for faster list item renderr

--- src/screens/home/game_platform/index.tsx
+++ src/screens/home/game_platform/index.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { RecyclerListView, DataProvider } from 'recyclerlistview';
+import FastImage from 'react-native-fast-image';
 import { NavigationInterface } from '../../types';
 import { useThemeContext } from '../../../theme';
-import ResponsiveImage from '../../../libs/responsiveImage';
 import Card from '../../../components/card';
+import platformsData from '../../../libs/platform.json';
+import { headerLayoutProvider } from '../recycler_list_view';
 
 import { Container, GamePlatformTitle, ImageClipper } from './styles';
 
-interface GameProp extends NavigationInterface {
+interface GameListHeaderProps extends NavigationInterface {
   testID?: string;
+}
+
+interface GameProp {
   id: number;
   abbreviation?: string;
   alternative_name?: string;
@@ -24,48 +30,77 @@ interface GameProp extends NavigationInterface {
   };
 }
 
-export default function GamePlatform(props: GameProp) {
+function GameListHeader(props: GameListHeaderProps) {
   const { colors } = useThemeContext();
 
-  const {
-    name,
-    abbreviation,
-    platform_logo: { url },
-    navigation
-  } = props;
+  const [state] = useState({
+    dataProvider: new DataProvider((r1, r2) => r1 !== r2).cloneWithRows(
+      platformsData
+    ),
+
+    layoutProvider: headerLayoutProvider()
+  });
 
   const handlePlatform = () => {
-    navigation.navigate('DetailScreen');
+    props.navigation.navigate('DetailScreen');
+  };
+
+  const renderGamePlatform = (_type: string, data: GameProp) => {
+    const {
+      name,
+      abbreviation,
+      platform_logo: { url }
+    } = data;
+
+    return (
+      <Container>
+        <Card
+          activeOpacity={0.7}
+          onPress={handlePlatform}
+          style={{
+            width: 65,
+            height: 65,
+            borderRadius: 65 / 2,
+            overflow: 'hidden',
+            borderWidth: 2,
+            borderColor: colors.ACTION_BG_COLOR,
+            justifyContent: 'center'
+          }}
+        >
+          <ImageClipper />
+
+          <FastImage
+            style={{
+              width: 55,
+              height: 55,
+              borderRadius: 55 / 2
+            }}
+            source={{
+              uri: `https:${url.replace('t_thumb', 't_thumb_2x')}`,
+              priority: FastImage.priority.normal
+            }}
+            resizeMode={FastImage.resizeMode.contain}
+          />
+        </Card>
+        <GamePlatformTitle>
+          {name.length > 12 ? abbreviation : name}
+        </GamePlatformTitle>
+      </Container>
+    );
   };
 
   return (
-    <Container>
-      <Card
-        activeOpacity={0.7}
-        onPress={handlePlatform}
-        style={{
-          width: 65,
-          height: 65,
-          borderRadius: 65 / 2,
-          overflow: 'hidden',
-          borderWidth: 2,
-          borderColor: colors.ACTION_BG_COLOR,
-          justifyContent: 'center'
-        }}
-      >
-        <ImageClipper />
-        <ResponsiveImage
-          width={55}
-          height={55}
-          imageUrl={`https:${url.replace('t_thumb', 't_thumb_2x')}`}
-          resizeMode="cover"
-          style={{ borderRadius: 55 / 2, zIndex: -1, marginTop: 3 }}
-          thumbnailBlurRadius={0.01}
-        />
-      </Card>
-      <GamePlatformTitle>
-        {name.length > 12 ? abbreviation : name}
-      </GamePlatformTitle>
-    </Container>
+    <RecyclerListView
+      style={{ flex: 1 }}
+      contentContainerStyle={{ paddingBottom: 0 }}
+      isHorizontal
+      dataProvider={state.dataProvider}
+      layoutProvider={state.layoutProvider}
+      rowRenderer={renderGamePlatform}
+      showsHorizontalScrollIndicator={false}
+      keyExtractor={(game: GameProp) => `${game.id}`}
+    />
   );
 }
+
+export default GameListHeader;
