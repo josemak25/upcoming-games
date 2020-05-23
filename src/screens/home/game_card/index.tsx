7f4e62e7cc064fb76d8179a0751d3f6cb1e71963
josemak25 <amakirij@gmail.com>
2020-05-23 04:08:44 +0100

    add game card component and implemented scrollable images

--- src/screens/home/game_card/index.tsx
+++ src/screens/home/game_card/index.tsx
@@ -1,14 +1,18 @@
 //@ts-nocheck
 import React from 'react';
-import { Dimensions } from 'react-native';
+import { Dimensions, TouchableOpacity } from 'react-native';
 import getGenres from '../../../utils/getGenres';
+import { EvilIcons } from '@expo/vector-icons';
 import AppIntroSlider from 'react-native-app-intro-slider';
 import { NavigationInterface } from '../../types';
 import { GameInterface } from '../../../store/game/types';
 import { useThemeContext } from '../../../theme';
 import { GameScreenshotInterface } from '../../../constants';
 import Card from '../../../components/card';
+import BookmarkIcon from '../../../../assets/icons/bookmark';
 import FastImage from 'react-native-fast-image';
+import LoveIcon from '../../../../assets/icons/love_icon';
+import applyScale from '../../../utils/applyScale';
 import GameScreenShotPagination from './pagination';
 
 import {
@@ -17,8 +21,8 @@ import {
   GamePlatformText,
   GamePlatformTitle,
   GameEngineTitle,
-  GamePaginationContainer,
-  GamePaginationText,
+  GameActionContainer,
+  GameActionLeftContainer,
   YearAndGenreSeparator,
   GameGenres
 } from './styles';
@@ -48,13 +52,12 @@ const Game = (props: GameProp) => {
 
   const handleFavorite = () => {};
 
+  const handleLove = () => {};
+
   const _renderItem = ({ item }) => {
     return (
       <FastImage
-        style={{
-          width: PHONE_FULL_WIDTH + 5,
-          height: PHONE_FULL_WIDTH - 100
-        }}
+        style={{ width: '100%', height: '100%' }}
         source={{
           uri: `https:${item.url.replace('t_thumb', 't_screenshot_med_2x')}`,
           priority: FastImage.priority.high
@@ -80,9 +83,9 @@ const Game = (props: GameProp) => {
         <GamePlatformImage>
           <FastImage
             style={{
-              width: 40,
-              height: 40,
-              borderRadius: 40
+              width: 30,
+              height: 30,
+              borderRadius: 30 / 2
             }}
             source={{
               uri: 'https://bit.ly/3cRTZjh',
@@ -92,10 +95,11 @@ const Game = (props: GameProp) => {
           />
         </GamePlatformImage>
         <GamePlatformText>
-          <GameEngineTitle>cyclone engine</GameEngineTitle>
-          <GamePlatformTitle>play station 3</GamePlatformTitle>
+          <GameEngineTitle>{name}</GameEngineTitle>
+          <GamePlatformTitle>cyclone engine</GamePlatformTitle>
         </GamePlatformText>
       </Card>
+
       <AppIntroSlider
         data={screenshots}
         renderItem={_renderItem}
@@ -109,13 +113,73 @@ const Game = (props: GameProp) => {
             screenshotsLength={screenshots.length}
           />
         )}
-        style={{
-          height: PHONE_FULL_WIDTH - 100,
-          width: PHONE_FULL_WIDTH
-        }}
+        horizontal
+        showsVerticalScrollIndicator={false}
+        style={{ width: PHONE_FULL_WIDTH }}
       />
+
+      <Card
+        activeOpacity={1}
+        onPress={handleGame}
+        style={{
+          width: '100%',
+          alignItems: 'flex-start',
+          paddingRight: 10,
+          paddingLeft: 5
+        }}
+      >
+        <GameActionContainer>
+          <GameActionLeftContainer>
+            <TouchableOpacity
+              onPress={handleLove}
+              style={{
+                width: 40,
+                height: 40,
+                justifyContent: 'center',
+                alignItems: 'center',
+                borderRadius: 40 / 2
+              }}
+            >
+              <LoveIcon fillColor={colors.INACTIVE_ICON_COLOR} />
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={handleLove}
+              style={{
+                width: 40,
+                height: 40,
+                justifyContent: 'center',
+                alignItems: 'center',
+                borderRadius: 40 / 2
+              }}
+            >
+              <EvilIcons
+                name="share-google"
+                size={30}
+                color={colors.INACTIVE_ICON_COLOR}
+              />
+            </TouchableOpacity>
+          </GameActionLeftContainer>
+          <TouchableOpacity
+            onPress={handleFavorite}
+            style={{
+              width: 40,
+              height: 40,
+              justifyContent: 'center',
+              alignItems: 'center',
+              borderRadius: 40 / 2
+            }}
+          >
+            <BookmarkIcon
+              width={25}
+              height="70%"
+              fillColor={colors.INACTIVE_ICON_COLOR}
+            />
+          </TouchableOpacity>
+        </GameActionContainer>
+        <GamePlatformText></GamePlatformText>
+      </Card>
     </Container>
   );
 };
 
-export default Game;
+export default React.memo(Game);
