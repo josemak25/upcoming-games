79e7d52e9fccf37f7a3c440e8b4f1946bf511b1e
josemak25 <amakirij@gmail.com>
2020-05-25 16:34:27 +0100

    add bookmark screen component styles

--- src/screens/bookmark/styles.ts
+++ src/screens/bookmark/styles.ts
@@ -2,7 +2,4 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
-  justify-content: center;
-  align-items: center;
-  background-color: ${({ theme }) => theme.colors.DARK_BG_COLOR};
 `;
