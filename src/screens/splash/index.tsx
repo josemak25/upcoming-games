c53d0d93f2415eeaa428fbf5c6f95c627fe16cb4
josemak25 <amakirij@gmail.com>
2020-05-31 01:55:23 +0100

    add new splash screen and animated app logo on app start up

--- src/screens/splash/index.tsx
+++ src/screens/splash/index.tsx
@@ -1,15 +1,34 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import Animated, { Easing } from 'react-native-reanimated';
 import { Container } from './styles';
 import AppIcon from '../../../assets/icons/app_icon';
+import bookmarkActions from '../../store/bookmark/actions';
+import { useStoreContext } from '../../store';
+import { BOOKMARK_ACTION_TYPES } from '../../store/bookmark/types';
+
+const AppAnimatedIcon = Animated.createAnimatedComponent(AppIcon);
 
 export default function SplashScreen({ navigation }) {
+  const { dispatch } = useStoreContext();
+
+  const [opacity] = useState(new Animated.Value(0));
+
+  const handleLogoTransition = () => {
+    Animated.timing(opacity, {
+      duration: 2000,
+      toValue: 1,
+      easing: Easing.inOut(Easing.ease)
+    }).start(() => navigation.navigate('HomeScreen'));
+  };
+
   useEffect(() => {
-    setTimeout(() => navigation.navigate('HomeScreen'), 2000);
+    handleLogoTransition();
+    bookmarkActions(dispatch, BOOKMARK_ACTION_TYPES.LOAD_CHECKED_BOOKMARKS);
   }, []);
 
   return (
     <Container>
-      <AppIcon />
+      <AppAnimatedIcon width="30%" height="30%" style={{ opacity }} />
     </Container>
   );
 }
