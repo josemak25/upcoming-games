643187eea096545b81d99756762ae52a7d07d547
josemak25 <amakirij@gmail.com>
2020-06-01 19:27:50 +0100

    add search view recycle layout builder

--- src/components/recycler_list_view/index.ts
+++ src/components/recycler_list_view/index.ts
@@ -8,7 +8,8 @@ export const ViewTypes = {
   GAME_LIST_HEADER: 'GAME_LIST_HEADER',
   GAME_LIST: 'GAME_LIST',
   GAME_PLATFORM: 'GAME_PLATFORM',
-  GAME_BOOKMARK: 'GAME_BOOKMARK'
+  GAME_BOOKMARK: 'GAME_BOOKMARK',
+  GAME_SEARCH: 'GAME_SEARCH'
 };
 
 export default function layoutProvider() {
@@ -17,12 +18,12 @@ export default function layoutProvider() {
     (type, dim) => {
       switch (type) {
         case ViewTypes.GAME_LIST:
-          dim.width = PHONE_FULL_WIDTH;
+          dim.width = Math.round(PHONE_FULL_WIDTH);
           dim.height = applyScale(550);
           break;
 
         case ViewTypes.GAME_LIST_HEADER:
-          dim.width = PHONE_FULL_WIDTH;
+          dim.width = Math.round(PHONE_FULL_WIDTH);
           dim.height = 95;
           break;
 
@@ -48,8 +49,34 @@ export function bookmarkLayoutProvider() {
   return new LayoutProvider(
     () => ViewTypes.GAME_BOOKMARK,
     (_type, dim) => {
-      dim.width = PHONE_FULL_WIDTH;
+      dim.width = Math.round(PHONE_FULL_WIDTH);
       dim.height = applyScale(550);
+    }
+  );
+}
+
+export function searchLayoutProvider() {
+  const adjustedMargin = Math.round(PHONE_FULL_WIDTH * 1000) / 1000 - 6;
+  const columnWidth = adjustedMargin / 3;
+
+  return new LayoutProvider(
+    () => ViewTypes.GAME_SEARCH,
+    (_type, dim, index) => {
+      switch (true) {
+        case index % 3 === 0:
+          dim.width = 3 * columnWidth;
+          dim.height = 300;
+          break;
+
+        case index % 2 === 0:
+          dim.width = 2 * columnWidth;
+          dim.height = 250;
+          break;
+
+        default:
+          dim.width = columnWidth;
+          dim.height = 250;
+      }
     }
   );
 }
