a613e915aafad6c97abd823d340b87a7ab75bd0c
josemak25 <amakirij@gmail.com>
2020-06-01 19:34:40 +0100

    add search page and implemented its functionalities

--- src/screens/search/index.tsx
+++ src/screens/search/index.tsx
@@ -2,16 +2,21 @@ import React, { useState } from 'react';
 import { StyleSheet, TouchableOpacity, Keyboard } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { BlurView } from '@react-native-community/blur';
+import { RecyclerListView, DataProvider } from 'recyclerlistview';
 import FastImage from 'react-native-fast-image';
+import API from '../../libs/api';
 import { NavigationInterface } from '../types';
 import { useThemeContext } from '../../theme';
 import { useStoreContext } from '../../store';
 import { MaterialIcons } from '@expo/vector-icons';
+import { searchLayoutProvider } from '../../components/recycler_list_view';
 import InputField from '../../components/input';
 import hexToRGB from '../../utils/hexToRGB';
+import getImageURL from '../../utils/getImageURL';
 import Card from '../../components/card';
 import SearchPlatforms from './platforms';
 import boxShadow from '../../utils/boxShadows';
+import LoadingGames from '../../components/LoadingGames';
 
 import {
   Container,
@@ -34,13 +39,81 @@ export default function SearchScreen(props: SearchScreenProp) {
 
   const [state, setState] = useState({
     searchWord: '',
-    selectedPlatform: '',
-    blueView: false
+    selectedPlatform: [],
+    blueView: false,
+    loading: false,
+    dataProvider: new DataProvider((r1, r2) => r1.id !== r2.id),
+    layoutProvider: searchLayoutProvider()
   });
 
-  const handleSubmit = () => setState({ ...state, blueView: false });
+  const handleSubmit = async () => {
+    if (!state.searchWord.length) {
+      return setState({ ...state, blueView: false });
+    }
+
+    setState({ ...state, loading: true });
+
+    const queryPath = `search "~ *'${state.searchWord}'*"; fields name, screenshots.*; where screenshots >= 1; limit 15;`;
+
+    const response = await API.post(queryPath);
+    const searchResults = await response.json();
+
+    setState({
+      ...state,
+      blueView: false,
+      loading: false,
+      dataProvider: state.dataProvider.cloneWithRows(searchResults)
+    });
+  };
 
   const handleFocus = () => setState({ ...state, blueView: true });
+
+  const handlePlatformSearch = async () => {
+    if (!state.selectedPlatform.length) return;
+
+    setState({ ...state, blueView: true, loading: true });
+
+    const queryPath = `fields screenshots.*, platforms; where platforms = (${state.selectedPlatform}) & screenshots >= 1; limit 15; exclude platforms;`;
+
+    const response = await API.post(queryPath);
+    const data = await response.json();
+
+    setState({
+      ...state,
+      blueView: false,
+      loading: false,
+      dataProvider: state.dataProvider.cloneWithRows([
+        ...state.dataProvider.getAllData(),
+        ...data
+      ])
+    });
+  };
+
+  const _renderSearchedGame = (_type: string, data: any, index: number) => (
+    <Card
+      style={{
+        flex: 1,
+        margin: 3,
+        marginTop: index === 0 ? 0 : 3,
+        backgroundColor: colors.DARK_BG_COLOR
+      }}
+    >
+      <FastImage
+        style={{
+          width: '100%',
+          height: '100%',
+          borderRadius: 3
+        }}
+        source={{
+          uri: getImageURL(data.screenshots),
+          priority: FastImage.priority.high
+        }}
+        resizeMode={FastImage.resizeMode.cover}
+      />
+    </Card>
+  );
+
+  const _renderFooter = () => state.loading && <LoadingGames />;
 
   return (
     <SafeAreaView
@@ -90,7 +163,10 @@ export default function SearchScreen(props: SearchScreenProp) {
             inputStyle={{ paddingRight: 50 }}
           >
             <SearchCancelIconContainer
-              onPress={() => setState({ ...state, searchWord: '' })}
+              onPress={() => {
+                if (!state.searchWord) return;
+                setState({ ...state, searchWord: '' });
+              }}
             >
               <MaterialIcons name="cancel" size={20} />
             </SearchCancelIconContainer>
@@ -103,9 +179,50 @@ export default function SearchScreen(props: SearchScreenProp) {
           </SearchContainerHeaderText>
         </SearchContainerHeader>
 
-        <SearchPlatforms
-          onPress={selectedPlatform => setState({ ...state, selectedPlatform })}
-        />
+        {!state.dataProvider.getSize() ? (
+          <SearchPlatforms
+            onPress={selectedPlatform =>
+              setState({
+                ...state,
+                selectedPlatform: [
+                  ...state.selectedPlatform,
+                  ...selectedPlatform
+                ]
+              })
+            }
+            handleCancel={setState}
+            handleSearch={handlePlatformSearch}
+          />
+        ) : (
+          <RecyclerListView
+            style={{
+              minHeight: 1,
+              minWidth: 1,
+              flex: 1,
+              width: '100%',
+              backgroundColor: colors.WHITE_BG_COLOR
+            }}
+            contentContainerStyle={{ marginLeft: 2, marginRight: 2 }}
+            dataProvider={state.dataProvider}
+            layoutProvider={state.layoutProvider}
+            rowRenderer={_renderSearchedGame}
+            renderFooter={_renderFooter}
+            showsVerticalScrollIndicator={false}
+            canChangeSize={true}
+            renderAheadOffset={4}
+            keyExtractor={(game: any) => `${game.id}`}
+            // scrollViewProps={{
+            //   refreshControl: (
+            //     <RefreshControl
+            //       refreshing={bookMarkState.isLoading}
+            //       onRefresh={onRefresh}
+            //       tintColor={colors.ACTION_BG_COLOR}
+            //       colors={[colors.ACTION_BG_COLOR]}
+            //     />
+            //   )
+            // }}
+          />
+        )}
 
         {state.blueView && (
           <BlurView
@@ -115,12 +232,19 @@ export default function SearchScreen(props: SearchScreenProp) {
             reducedTransparencyFallbackColor="white"
           >
             <TouchableOpacity
-              style={{ flex: 1 }}
+              style={{
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center'
+              }}
               onPress={() => {
+                if (state.loading) return;
                 setState({ ...state, blueView: false });
                 Keyboard.dismiss();
               }}
-            />
+            >
+              {state.loading ? <LoadingGames /> : null}
+            </TouchableOpacity>
           </BlurView>
         )}
       </Container>
