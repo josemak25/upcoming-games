9fd658b2b32ff9ec8ae3996b22e999f7ec95237a
josemak25 <amakirij@gmail.com>
2020-06-01 19:34:11 +0100

    add change to get image uri method

--- src/utils/getImageURL.ts
+++ src/utils/getImageURL.ts
@@ -1,8 +1,10 @@
 import { GameScreenshotInterface } from '../constants';
 
 export default function getImageURL(screenshots: GameScreenshotInterface[]) {
-  const randomImage = Math.floor(Math.random() * screenshots.length);
-  const { url } = screenshots[randomImage];
-  const largeImageURL = `https:${url.replace('t_thumb', 't_screenshot_huge')}`;
+  const { url } = screenshots[0];
+  const largeImageURL = `https:${url.replace(
+    't_thumb',
+    't_screenshot_med_2x'
+  )}`;
   return largeImageURL;
 }
