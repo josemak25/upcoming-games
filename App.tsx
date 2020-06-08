8051640145b09fb17a1b59a469c673c076696496
josemak25 <amakirij@gmail.com>
2020-06-08 01:24:47 +0100

    add signup image preload for faster rendering on signup page

--- App.tsx
+++ App.tsx
@@ -1,9 +1,15 @@
 import React, { useState } from 'react';
+import FastImage from 'react-native-fast-image';
 import AppLoading from './src/components/AppLoading';
 import AppRouter from './src';
+import { signupBackgroundLink } from './src/constants';
 
 export default function App() {
   const [isAppReady, setIsAppReady] = useState(false);
+
+  FastImage.preload([
+    { uri: signupBackgroundLink, priority: FastImage.priority.high }
+  ]);
 
   return isAppReady ? (
     <AppRouter />
